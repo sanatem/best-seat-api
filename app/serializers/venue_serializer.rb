3f79e77487963c6962c6fd53991f79380d8b45de
Santiago Pravisani <santiagopravisani@gmail.com>
2020-03-31 23:28:34 +0100

    Seeds and serializer for availability

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -2,6 +2,10 @@ class VenueSerializer
   include FastJsonapi::ObjectSerializer
   attributes :rows, :columns
 
+  attribute :available_seats do |obj|
+    obj.available_seats.map(&:public_id)
+  end
+
   # attribute :seats do |obj|
   #   SeatSerializer.new(obj.seats)
   # end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ puts 'Populating Venue with seats..'
 venue = Venue.create(rows: 2, columns: 5)
 
 puts 'Populating seats..'
-venue.create_sits
+venue.create_seats(true)
